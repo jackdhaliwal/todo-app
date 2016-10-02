88e17b9bb17056c4bf8a52d74bc699e2c95e52b3
jackdhaliwal <brinder.dhaliwal13@gmail.com>
2016-10-01 21:46:50 -0700

    final steps of creating destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,15 @@ def new
 	@task = Task.new
 end
 def create
-  @task = Task.create(task_params)
-  redirect_to root_path
-end
+      @task = Task.create(task_params)
+      @tasks = Task.all
+    end
 def task_params
   params.require(:task).permit(:title, :note, :completed)
+end
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  @tasks = Task.all
 end
 end
