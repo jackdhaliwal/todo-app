f39d4d22b83e76ead85196c523e83f8736ab516a
jackdhaliwal <brinder.dhaliwal13@gmail.com>
2016-10-01 19:27:20 -0700

    Initial Commit of Todo App

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,12 @@
+class TasksController < ApplicationController
+def new
+	@task = Task.new
+end
+def create
+  @task = Task.create(task_params)
+  redirect_to root_path
+end
+def task_params
+  params.require(:task).permit(:title, :note, :completed)
+end
+end
