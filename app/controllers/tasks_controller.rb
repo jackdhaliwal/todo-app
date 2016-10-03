7ab834531f54f0dc87dcd9fdb919fae2cc8cf0ef
jackdhaliwal <brinder.dhaliwal13@gmail.com>
2016-10-03 06:39:22 -0700

    functioning destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,47 @@
+
 class TasksController < ApplicationController
-def new
-	@task = Task.new
-end
-def create
-      @task = Task.create(task_params)
-      @tasks = Task.all
+    def new
+      @task = Task.new
+      render :show_form
     end
-def task_params
-  params.require(:task).permit(:title, :note, :completed)
+
+def create
+  @task = Task.new(task_params)
+  save_task
 end
-def destroy
+
+    def edit
+      @task = Task.find(params[:id])
+      render :show_form
+    end
+
+
+def update
   @task = Task.find(params[:id])
-  @task.destroy
-  @tasks = Task.all
+  @task.assign_attributes(task_params)
+  save_task
 end
+
+  def destroy
+    @task = Task.find(params[:id])
+    authorize! :destroy, @task
+    @task.destroy
+    @tasks = Task.all
+  end
+
+ private
+
+  def save_task
+    if @task.save
+      @tasks = Task.all
+      render :hide_form
+    else
+      render :show_form
+    end
+  end
+
+
+  def task_params
+    params.require(:task).permit(:title, :note, :completed)
+  end
 end
